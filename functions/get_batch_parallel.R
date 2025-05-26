93d1a63a7129ee77c02cf1cd00cf4fe83239bb91
Cai <cai076@csiro.au>
2025-05-26 12:45:01 +1000

    refine the batch subsetting function

--- functions/get_batch_parallel.R
+++ functions/get_batch_parallel.R
@@ -1,27 +1,32 @@
-#'@param batchnum the number for the order of the 'batch' or the 'subset'; can be a subset of grid cells or temporal periods.
-#'@param entire entire list of arguments to separate out; most likely a vector 
-#'@param size size of the batch
+#'@Desc *Function to subset the large-scale task by the batch number and the batch size*
+#'@Desc *The task to be subset can be grid cells and/or timesteps*
+
+#'@param batchnum the number for the 'batch' / 'subset'; normally passed by the cluster argument (numeric)
+#'@param entire entire list of values / arguments to subset into batches; a vector 
+#'@param size size of the batch (numeric)
 
 get_batch_parallel = function(batchnum, entire, size) {
   
-  # Total number of arguments to be subsetted
+  # Total number of values / arguments to be subsetted
   N_args = length(entire)
   
   # Start and the end index for the batch given the input batch number
   start_ind = size * (batchnum - 1) + 1
   upper_bound = size * batchnum
   
-  # If the end index of the batch is greater than the total number: 
-  # the end index would be the final number of the argument
   if (upper_bound > N_args & start_ind <= N_args) {
+    # If the upper bound of the batch is greater than the total number: 
+    # the upper bound would be the final index of the entire values
     batch_args = entire[start_ind : N_args]
+    
   } else if (upper_bound <= N_args) {
-    # If the end index of the batch is within the total number of argument 
-    # Subset the full batch
+    # If the upper bound of the batch is within the total number of values 
+    # It will be a normal full subset of values with the desired size 
     end_ind = size * batchnum
     batch_args = entire[start_ind : end_ind]
+    
   } else if (start_ind > N_args) {
-    # If start index is greater than the total number of argument: can't run, pass*
+    # If start index is greater than the total number of values: it's an error, stop.
     stop("Batch index exceeds total points, stop")
   }
   
